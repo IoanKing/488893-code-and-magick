22e64c9bd4182870d06c1ed2c9912cd6a98f385c
Ivan Suslov <ioankingisepp@mail.ru>
2019-01-12 12:15:50 +0300

    Учебный проект: выкладываем фото

---
+++ js/avatar.js
@@ -0,0 +1,28 @@
+// Файл avatar.js
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var fileChooser = document.querySelector('.upload input[type=file]');
+  var preview = document.querySelector('.setup-user-pic');
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
